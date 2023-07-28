db533941e39d7d0086d70bf843f59b193d1d7943
Gabriel Lima <gabriellima0129@outlook.com>
2023-07-28 02:04:37 -0300

    atualização nos projetos

--- src/PaginaInicial/SecondMid/index.jsx
+++ src/PaginaInicial/SecondMid/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css"
 
+
 const SecondMid = () => {
     return (
         <>
@@ -15,45 +16,46 @@ const SecondMid = () => {
                       <div id="container2">
 
                         
-                        <div id="card" className="bg-black"> 
-                          <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-                          deserunt mollit anim id est laborum."
-                          </p>
-                        </div>  
+                      <div id="card" className="bg-black"> 
+                        <h1>Food Commerce</h1>
+                          <a href="https://food-commerce-delta.vercel.app/" target="blank" rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/Nh8Jzq1.png" alt="Food Commerce"/>
+                          </a>
+
+                          <a href="https://github.com/Gabriellima784/food-commerce" target="_blank" rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/ecyWOWs.png" alt="Github" className="logo" />
+                          </a>
+                          <p className="hover-text">Loja de pedidos online com BackEnd produzido em NodeJS e FrontEnd em React.</p>
+                      </div>  
                         
-                        <div id="card2" className="bg-black"> 
-                          <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-                          deserunt mollit anim id est laborum."
-                          </p>
+                        
+
+                      <div id="card2" className="bg-black"> 
+                        <h1>Password Generator</h1>
+                          <a href="https://gabriellima784.github.io/password-generator/" target="blank" rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/n1pWovM.png" alt="Password Generator"/>
+                          </a>
+                          <a href="https://github.com/Gabriellima784/password-generator" target="_blank" rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/ecyWOWs.png" alt="Github" className="logo" />
+                          </a>
+                          <p className="hover-text2">Projeto construido em JavaScript Vanilla, para estudo de lógica de programação aplicada a casos reais.</p>
+                      </div>  
+
+                       
+
+
+                        
+                        <div id="card3" className="bg-black">
+                        <h1>Batman Site</h1> 
+                        <a href="https://gabriellima784.github.io/Batman/" target="blank" rel="noopener noreferrer">
+                          <img src="https://i.imgur.com/qgNSq7N.png" alt="Batman Site"/>
+                        </a>
+                        <a href="https://github.com/Gabriellima784/Batman" target="_blank" rel="noopener noreferrer">
+                          <img src="https://i.imgur.com/ecyWOWs.png" alt="Github" className="logo" />
+                        </a>
+                        <p className="hover-text2">Meu primeiro site com HTML,CSS e JavaScript construído, nele também foi aplicado um BackEnd para exibição dos comentários em tempo real na tela.</p>
                         </div>  
-    
-                        <div id="card3" className="bg-black"> 
-                          <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-                          deserunt mollit anim id est laborum."
-                          </p>
-                        </div>  
-    
-                        <div id="card4" className="bg-black"> 
-                          <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-                          deserunt mollit anim id est laborum."
-                          </p>
-                        </div>  
-    
-    
-    
-    
+
               </div>
             </div>
           <br></br>
